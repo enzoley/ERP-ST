fb2f81adc0107e125be3990f7479943e8e55968a
enzo <enzoleroyer2@gmail.com>
2024-05-16 11:59:00 +0200

    restriction des pages selon situation

--- www/js/account.js
+++ www/js/account.js
@@ -5,7 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
             if (data.loggedIn) {
                 const path = window.location.pathname;
                 const pageName = path.split('/').pop();
-                console.log('Logged in as:', data.user);
+                if ((pageName == 'accueilEtudiant.html' || pageName == 'contactEtudiant.html' || pageName == 'rappelEtudiant.html' || pageName == 'suiviEtudiant.html' || pageName == 'visiteEtudiant.html') && data.user.situation == 'etudiant') {
+                    console.log('Logged in as:', data.user);
+                } else if ((pageName == 'accueilEntreprise.html' || pageName == 'contactEntreprise.html' || pageName == 'rappelEntreprise.html' || pageName == 'suiviEnteprise.html' || pageName == 'visiteEntreprise.html' || pageName == 'partenariat.html') && data.user.situation == 'entreprise') {
+                    console.log('Logged in as:', data.user);
+                } else if ((pageName == 'accueilResponsablePedagogique.html' || pageName == 'suiviResponsablePedagogique.html' || pageName == 'visiteResp.html') && data.user.situation == 'pedagogique') {
+                    console.log('Logged in as:', data.user);
+                } else {
+                    console.log('Vous n\'êtes pas autorisé à accéder à cette page');
+                    const situation = data.user.situation;
+                    if (situation == "etudiant") {
+                        window.location.href = "accueilEtudiant.html";
+                    } else if (situation == "entreprise") {
+                        window.location.href = "accueilEntreprise.html";
+
+                    } else {
+                        window.location.href = "accueilResponsablePedagogique.html";
+                    }
+                }
             } else {
                 console.log('Not logged in');
                 window.location.href = 'index.html';
