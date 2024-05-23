07a023e89d7ef6b53d0e5ae100a0e33fd68b1941
enzo <enzoleroyer2@gmail.com>
2024-05-23 10:41:46 +0200

    gestion suivi resp

--- www/src/suiviResp.ts
+++ www/src/suiviResp.ts
@@ -1,6 +1,37 @@
 const suiviDivResp = document.getElementById('suivi') as HTMLDivElement;
 const etuSelectorResp = document.getElementById('selectEtu') as HTMLSelectElement;
 
+function month2(mois: number) {
+    switch (mois) {
+        case 0:
+            return 'Janvier';
+        case 1:
+            return 'Février';
+        case 2:
+            return 'Mars';
+        case 3:
+            return 'Avril';
+        case 4:
+            return 'Mai';
+        case 5:
+            return 'Juin';
+        case 6:
+            return 'Juillet';
+        case 7:
+            return 'Août';
+        case 8:
+            return 'Septembre';
+        case 9:
+            return 'Octobre';
+        case 10:
+            return 'Novembre';
+        case 11:
+            return 'Décembre';
+        default:
+            return 'Mois inconnu';
+    }
+}
+
 async function loadEtu() {
     try {
         const reponseCompte = await fetch('http://localhost:3000/check-login');
@@ -18,16 +49,56 @@ async function loadEtu() {
             body: JSON.stringify({ idResp: id })
         });
         const etudiants = await response.json();
-        console.log(etudiants);
-        etudiants.forEach((etu: string) => {
+        etudiants.forEach((etu: string, index: number) => {
             const option = document.createElement('option');
             option.value = etu;
-            option.textContent = etu
+            option.textContent = etu;
             etuSelectorResp.appendChild(option);
         });
+        if (etuSelectorResp.options.length > 0) {
+            etuSelectorResp.options[0].selected = true;
+        }
+        loadSuiviResp();
     } catch (error) {
         console.error('Erreur lors du chargement des étudiants :', error);
     }
+}
+
+etuSelectorResp.addEventListener("change", loadSuiviResp);
+
+function loadSuiviResp() {
+    suiviDivResp.innerHTML = "";
+    const etu = etuSelectorResp.value;
+    const etuName = etu.split(' ').join('');
+    console.log(etuName);
+    fetch('http://localhost:3000/suivi-etu', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: etuName })
+    })
+        .then(response => response.json())
+        .then(suivi => {
+            suivi.forEach((suivi: { id: string; mois: string; annee: string; taches: string; commentaires: string; idEtu: string, idResp: string, idEnt: string }) => {
+                const moisInt = parseInt(suivi.mois);
+                const monthName = month2(moisInt);
+                const suiviCard = document.createElement('div');
+                suiviCard.classList.add('card', 'mb-5');
+                suiviCard.innerHTML = `
+                <div class="card-header">
+                    ${monthName} ${suivi.annee}
+                </div>
+                <div class="card-body">
+                    <p class="card-title"><b>Tâches : </b></p>
+                    <p class="card-text">${suivi.taches}</p>
+                    <p class="card-title"><b>Commentaires : </b></p>
+                    <p class="card-text">${suivi.commentaires}</p>
+                </div>
+            `;
+                suiviDivResp.appendChild(suiviCard);
+            });
+        });
 }
 
 loadEtu();
