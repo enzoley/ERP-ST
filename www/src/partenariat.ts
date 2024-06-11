736f2176de78ce05a1b3cb7e742a72e49c5137ff
enzo <enzoleroyer2@gmail.com>
2024-06-11 14:18:49 +0200

    ajout bouton send partenariat

--- www/src/partenariat.ts
+++ www/src/partenariat.ts
@@ -1,6 +1,7 @@
 const partenariat = document.getElementById('partenariat') as HTMLFormElement;
 const emailInput = document.getElementById('inputEmailPart') as HTMLInputElement;
 const formDiv = document.getElementById('form') as HTMLDivElement;
+const sendButton = document.getElementById('sendButton') as HTMLButtonElement;
 
 document.addEventListener('DOMContentLoaded', async () => {
     const reponseCompte = await fetch('https://entreprises.startechnormandy.com/check-login');
@@ -33,6 +34,36 @@ document.addEventListener('DOMContentLoaded', async () => {
             `;
                 formDiv.appendChild(suiviCard);
             }
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        console.error('Erreur :', error);
+        alert('Une erreur est survenue. Veuillez réessayer.');
+    }
+});
+
+sendButton.addEventListener('click', async (event) => {
+    const reponseCompte = await fetch('https://entreprises.startechnormandy.com/check-login');
+    const compteRes = await reponseCompte.json();
+    if (!compteRes.loggedIn) {
+        window.location.href = 'index.html';
+        return;
+    }
+    const id = compteRes.user.id;
+    try {
+        const response = await fetch('https://entreprises.startechnormandy.com/add-ent-par', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ idEnt: id })
+        });
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Ajout réussi');
+            window.location.reload();
         } else {
             alert(data.message);
         }
